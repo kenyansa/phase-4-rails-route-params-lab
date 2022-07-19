782bd8362d8b54274e90e01580c886e08588c30d
Kenyansa <kenyansajustine@gmail.com>
2022-07-19 07:24:24 +0300

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,27 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def first_student
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
+  def second_student
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
+  # def index
+  #   students = if params[:first_name]
+  #                Student.by_name(params[:last_name])
+  #              else
+  #                Student.all
+  #              end    
+  #   render json: students
+  # end
+  # def show
+  #   student = Student.find(params[:id])
+  #   render json: student
+  # end
+
 end
